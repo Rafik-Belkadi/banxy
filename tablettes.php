515d4edad9a5ffa436c6ea6a9ade89a5e2abf328
Mohamed Belkadi <mohamed.belkadi@MacBook-Pro-de-Mohamed.local>
2019-07-23 14:26:32 +0100

    fix

--- tablettes.php
+++ tablettes.php
@@ -9,7 +9,7 @@ include 'includes/header.php';
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800"></h1>
-        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i>Exporter Rapport d'activités</a>
+        <a href="actions/export-forms.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i>Exporter BDD</a>
     </div>
 
     <!-- Content Row -->
