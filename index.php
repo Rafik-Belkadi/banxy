d6a7ef0472c5081342d36b025b740a3a0c41cbad
Mohamed Belkadi <mohamed.belkadi@MacBook-Pro-de-Mohamed.local>
2019-07-23 14:22:41 +0100

    changed index

--- index.php
+++ index.php
@@ -10,9 +10,24 @@ include 'models/User.php';
   <!-- Page Heading -->
   <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-    <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+    <a href="actions/export.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Exporter BDD</a>
   </div>
+  
+<?php 
 
+$Form = new Form($db);
+$User = new User($db);
+
+$result = $Form->getAllForms();
+
+
+$numberInteractions = $User->getInteractionRowsNumber();
+$numberWeekInteractions = $User->getCurrentWeekInteractionRowsNumber();
+
+$numberForms = $Form->getInteractionRowsNumber();
+$numberWeekForms = $Form->getCurrentWeekFormsNumber();
+?>
+  
   <!-- Content Row -->
   <div class="row">
 
@@ -22,8 +37,8 @@ include 'models/User.php';
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Interactions</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $numberInteractions ?></div>
             </div>
             <div class="col-auto">
               <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -39,9 +54,10 @@ include 'models/User.php';
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+              <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Interactions (Hebdo) </div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $numberWeekInteractions ?></div>
             </div>
+            
             <div class="col-auto">
               <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
             </div>
@@ -56,14 +72,14 @@ include 'models/User.php';
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
+              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">BDD</div>
               <div class="row no-gutters align-items-center">
                 <div class="col-auto">
-                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $numberForms ?></div>
                 </div>
                 <div class="col">
                   <div class="progress progress-sm mr-2">
-                    <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar bg-info" role="progressbar" style="width: 100%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
                 </div>
               </div>
@@ -82,8 +98,8 @@ include 'models/User.php';
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+              <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">BDD (Hebdo)</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $numberWeekForms ?></div>
             </div>
             <div class="col-auto">
               <i class="fas fa-comments fa-2x text-gray-300"></i>
@@ -101,7 +117,7 @@ include 'models/User.php';
       <div class="card shadow mb-4">
         <!-- Card Header - Dropdown -->
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-          <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+          <h6 class="m-0 font-weight-bold text-primary">Trafic overview</h6>
           <div class="dropdown no-arrow">
             <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
@@ -120,6 +136,8 @@ include 'models/User.php';
           <div class="chart-area">
             <canvas id="myAreaChart"></canvas>
           </div>
+          <hr>
+          analyse de performance sur l'ensemble des touchpoints
         </div>
       </div>
     </div>
@@ -129,7 +147,7 @@ include 'models/User.php';
       <div class="card shadow mb-4">
         <!-- Card Header - Dropdown -->
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-          <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
+          <h6 class="m-0 font-weight-bold text-primary"> Rubrique overview</h6>
           <div class="dropdown no-arrow">
             <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
@@ -150,14 +168,19 @@ include 'models/User.php';
           </div>
           <div class="mt-4 text-center small">
             <span class="mr-2">
-              <i class="fas fa-circle text-primary"></i> Direct
+              <i class="fas fa-circle text-primary"></i> Gestion Mobile
             </span>
             <span class="mr-2">
-              <i class="fas fa-circle text-success"></i> Social
+              <i class="fas fa-circle text-success"></i> Avantages
             </span>
             <span class="mr-2">
-              <i class="fas fa-circle text-info"></i> Referral
+              <i class="fas fa-circle text-info"></i> Cartes <br>
             </span>
+            <span class="mr-2">
+              <i class="fas fa-circle text-dark"></i> Écoute
+            </span>
+            <hr>
+            analyse des intérêts portés sur l'offre par rubrique
           </div>
         </div>
       </div>
@@ -165,53 +188,60 @@ include 'models/User.php';
   </div>
   <!-- Content Row -->
 
-
-<?php 
-
-  $Form = new Form($db);
-  $User = new User($db);
-
-  $result = $Form->getAllForms();
-  
-
-?>
-
   <!-- DataTales Example -->
   <div class="card shadow mb-4">
     <div class="card-header py-3">
-      <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+      <h6 class="m-0 font-weight-bold text-primary">BDD</h6>
     </div>
     <div class="card-body">
       <div class="table-responsive">
         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
           <thead>
             <tr>
+              <th>Id</th>
               <th>Nom</th>
               <th>Prénom</th>
               <th>E-mail</th>
               <th>Civilité</th>
-              <th>Date d'ajout</th>
+              <th>Mobile</th>
               <th>Localisation</th>
+              <th>Date d'ajout</th>
             </tr>
           </thead>
           <tfoot>
             <tr>
+              <th>Id</th>
               <th>Nom</th>
               <th>Prénom</th>
               <th>E-mail</th>
               <th>Civilité</th>
-              <th>Date d'ajout</th>
+              <th>Mobile</th>
               <th>Localisation</th>
+              <th>Date d'ajout</th>
             </tr>
           </tfoot>
           <tbody>
+          <?php 
+          
+            while($data = $result->fetch()){
+              
+              $user_id = $data['user_id'];
+              $localisation = $User->getUser($user_id)->fetch()['localisation'];
+              
+              echo '
+              <td>' . $data['id'] . '</td>
+              <td>' . $data['first_name'] . '</td>
+              <td>' . $data['last_name'] . '</td>
+              <td>' . $data['email'] . '</td>
+              <td>' . $data['civilite'] . '</td>
+              <td>' . $data['mobile'] . '</td>
+              <td>' . $localisation . '</td>
+              <td>' . $data['created_at'] . '</td>
+              ';
+            }
+          ?>
             <tr>
-              <td>Tiger Nixon</td>
-              <td>System Architect</td>
-              <td>Edinburgh</td>
-              <td>61</td>
-              <td>2011/04/25</td>
-              <td>$320,800</td>
+              
             </tr>
 
           </tbody>
